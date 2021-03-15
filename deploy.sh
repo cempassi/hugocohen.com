5663dbc21a87378e970df796fe38820fa1d2a718
Cedric M'Passi <cempassi@student.42.fr>
2021-03-15 13:31:08 +0100

    Remove deploy.sh

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-make -C Frontend
-docker build -t cempassi/vimeo-app-backend ./Backend 
-docker build -t cempassi/vimeo-app-frontend ./Frontend 
-docker push cempassi/vimeo-app-frontend
-docker push cempassi/vimeo-app-backend
-ssh root@'api.hugocohen.com' 'cd site_hugo; ./refresh.sh'
